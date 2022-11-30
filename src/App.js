825f8722563b9069ae78b2295bce33359c938d65
devsticks <devsticks@gmail.com>
2022-11-30 18:12:00 +0200

    To vid 1.3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { SplitScreen } from "./SplitScreen";
+
+const LeftHandComponent = ({ name }) => {
+    return <h1>{name}</h1>;
+};
+
+const RightHandComponent = ({message}) => {
+	return <h2>{message}</h2>;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  	return (
+    	<SplitScreen 
+			leftWeight={2}
+			rightWeight={1}
+    	>
+			<LeftHandComponent name="Dev"/>
+			<RightHandComponent message="Hi!"/>
+		</SplitScreen>
+  	);
 }
 
 export default App;
